4953ea47b27129bf0cd66b5ba5db6acb6700527d
maks <mamonov.maksim.20003@gmail.com>
2023-02-04 17:49:19 +0500

    доработка 0.1

--- scripts422.sql
+++ scripts422.sql
@@ -1,21 +1,24 @@
+INNER JOIN human ON human.id = car.id;
 CREATE TABLE human(
                       name TEXT PRIMARY KEY,
                       age INTEGER PRIMARY KEY,
-                      license BOOLEAN PRIMARY KEY
+                      license BOOLEAN PRIMARY KEY,
+                      id INTEGER PRIMARY KEY
 );
 
 CREATE TABLE car(
                     stamp TEXT PRIMARY KEY,
                     model TEXT PRIMARY KEY,
-                    cost TEXT PRIMARY KEY
+                    cost INTEGER PRIMARY KEY,
+                    id INTEGER PRIMARY KEY
 );
 
-SELECT car, human ON human = car;
 
-
+INNER JOIN faculty ON faculty.id = student.faculty_id;
 SELECT student.name, student.age, faculty.name
-FROM student INNER JOIN united;
+FROM student INNER JOIN faculty;
+
 
 SELECT student.name, student.age, faulty.name
-FROM student INNER JOIN studentsByAvatars ON
+FROM student INNER JOIN faculty ON
         avatar.id = student.id;
